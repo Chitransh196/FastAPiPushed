24ac747a5d0e8dceaf21645013b726d87d3cdc58
Chitransh196 <chitranshraizada@gmail.com>
2025-12-18 17:28:10 +0530

    corrected reltionship

--- models.py
+++ models.py
@@ -1,17 +1,20 @@
 from sqlalchemy import Column, Integer, String, ForeignKey
 from Student_project import Base, engine
 from pydantic import BaseModel
+from sqlalchemy.orm import relationship
 
 class Department(Base):
     __tablename__ = "departments"
     id = Column(Integer, primary_key=True)
     name = Column(String)
+    students = relationship("Student", back_populates="department" )
 
 class Student(Base):
     __tablename__ = "students"
     id = Column(Integer, primary_key=True)
     name = Column(String)
     dept_id = Column(Integer, ForeignKey("departments.id"))
+    department = relationship("Department", back_populates="students")
 
 class DepartmentCreate(BaseModel):
     name: str
